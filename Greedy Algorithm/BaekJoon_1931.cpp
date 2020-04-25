407e44e30058cbd7075e39f3a85ba23d262602fb
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-04-25 17:14:06 +0900

    Update BaekJoon_1931.cpp

--- Greedy Algorithm/BaekJoon_1931.cpp
+++ Greedy Algorithm/BaekJoon_1931.cpp
@@ -1,42 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 bool compare(const pair<int, int>& a, const pair<int, int>& b);
 int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-
-	int s, f; 
-	int answer = 0;
-	int n;
+	vector<pair<int, int>> w;
+	int n, s,f;
 	cin >> n;
-
-	vector <pair <int, int>> v;
 	for (int i = 0; i < n; i++) {
 		cin >> s >> f;
-		v.push_back(make_pair(s, f));
+		w.push_back(make_pair(s, f));
 	}
-	sort(v.begin(), v.end(), compare);
-
-	int min = v[0].second;
-	answer++;
-
+	sort(w.begin(), w.end(),compare);
+	int tmp = w[0].second;
+	int answer = 1;
 	for (int i = 1; i < n; i++) {
-		if (v[i].first > min) {
-			min = v[i].second;
+		if (w[i].first >= tmp) {
 			answer++;
+			tmp = w[i].second;
 		}
 	}
-	cout << answer;
-
-	return 0;
+	cout << answer << '\n';
 }
 bool compare(const pair<int, int>& a, const pair<int, int>& b) {
-	if (a.second == b.second) {//if finish times are same
-		return a.first < b.first; //ascending order to start time
-	}
-	return a.second < b.second; //ascending oreder to finish time
+	if (a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
 }
