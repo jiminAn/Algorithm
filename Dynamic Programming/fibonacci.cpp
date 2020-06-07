702a5487712498d1f6cd9e74cfdceb364352b66e
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-06-07 17:12:06 +0900

    Update fibonacci.cpp
    add optimize solution

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -3,40 +3,77 @@
 
 using namespace std;
 
-vector<int> memo(100);
-int recursive_fibo(int n);
-int memo_fibo(int n);
-void memo_init(int n);
 
+int fibo_recursive(int n); // O(2^n)
+int fibo_dp(int n); // O(n)
+
+
+/* using power of the matrix
+fn-1+ fn = | 0 1 |n-1  x |f0|
+           | 1 1 |       |f1|
+*/
+int fibo_dnc(int n); // O(logn)
+void multiply(int fibo[2][2], int m[2][2]);
+void power(int fibo[2][2], int n);
 
 
 int main(void){
-    int ans = recursive_fibo(20);
+    int ans;
+    ans = fibo_recursive(20);
     cout << ans << '\n';
-    
-    memo_init(20);
-    ans = memo_fibo(20);
+    ans = fibo_dp(6);
+    cout << ans << '\n';
+    ans = fibo_dnc(6);
     cout << ans << '\n';
     
     return 0;
 }
 
-int recursive_fibo(int n){
+int fibo_recursive(int n){
     if( n == 0)
         return 0;
     else if ( n == 1)
         return 1;
     else
-        return recursive_fibo(n-1) + recursive_fibo(n-2);
+        return fibo_recursive(n-1) + fibo_recursive(n-2);
     
 }
-void memo_init(int n){
-    for(int i = 0; i <= n; i++)
-        memo[i] = 0;
-    memo[1] = 1;
+\
+int fibo_dp(int n){
+    int  f[100];
+    f[0] = 0;
+    f[1] = 1;
+    for(int i = 2; i <= n; i++)
+        f[i] = f[i-1] + f[i-2];
+    return f[n];
 }
-int memo_fibo(int n){
-    if(n >= 2 && memo[n] == 0)
-        memo[n] = memo_fibo(n-1) + memo_fibo(n-2);
-    return memo[n];
+int fibo_dnc(int n){
+    int fibo[2][2] = {{1,1},{1,0}};
+    if(n == 0)
+        return 0;
+    power(fibo, n-1);
+    return fibo[0][0];
 }
+void power(int fibo[2][2], int n){
+    if( n == 0 || n == 1)
+        return;
+    int m[2][2] = {{1,1},{1,0}};
+    
+    power(fibo, n/2);
+    multiply(fibo, fibo);
+    if( n % 2 != 0)
+        multiply(fibo, m);
+}
+void multiply(int fibo[2][2], int m[2][2])
+{
+  int x =  fibo[0][0]*m[0][0] + fibo[0][1]*m[1][0];
+  int y =  fibo[0][0]*m[0][1] + fibo[0][1]*m[1][1];
+  int z =  fibo[1][0]*m[0][0] + fibo[1][1]*m[1][0];
+  int w =  fibo[1][0]*m[0][1] + fibo[1][1]*m[1][1];
+  
+  fibo[0][0] = x;
+  fibo[0][1] = y;
+  fibo[1][0] = z;
+  fibo[1][1] = w;
+}
+  
