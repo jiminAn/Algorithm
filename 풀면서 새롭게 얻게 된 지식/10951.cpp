dc4f373d4956bbabe3b122a6faa3f1c1ecd5d680
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-07-08 00:48:39 +0900

    Update 10951.cpp

--- 풀면서 새롭게 얻게 된 지식/10951.cpp
+++ 풀면서 새롭게 얻게 된 지식/10951.cpp
@@ -3,13 +3,9 @@
 using namespace std;
 
 int main(void){
-    int A,B;
+    int a,b;
     
-    int sum_arr[100];
-    for(int i = 0; i < 5; i++){
-        cin >> A >> B;
-        sum_arr[i] = A + B;
-    }
-    for(int i = 0; i < 5; i++)
-        cout << sum_arr[i] << '\n';
+    while( cin >> a >> b)
+        cout << a + b << '\n';
+    return 0;
 }
