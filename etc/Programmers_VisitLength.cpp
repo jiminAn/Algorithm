59976a4d53ae1df2446e2d23bdddb0a5ecfb4643
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-09-02 19:22:25 +0900

    Update Programmers_VisitLength.cpp

--- etc/Programmers_VisitLength.cpp
+++ etc/Programmers_VisitLength.cpp
@@ -5,52 +5,30 @@ int solution(string dirs)
 {
 	int answer = 0;
     bool check[11][11][11][11] = {false};
+    int dy[4] = {0,0,1,-1}; // R/L/U/D
+    int dx[4] = {1,-1,0,0}; // R/L/U/D
     int x,y;
-    x = y = 5; //실제 좌표 상의 (0,0) -> 배열 index (5,5)
-    
-    /*(x1,y1)->(x2,y2)로 이동할 경우 지나간 길 경로 체크시 방향성이 없는 문제이므로
-    (x2,y2)->(x1,y1)도 체크해주어야 함*/
+    int dir;
+    int tmp_x, tmp_y;
+    x = y = 5; //실제 좌표 상의 (0,0)-> 배열 index (5,5)
     for(int i = 0; i < dirs.length(); i++){
-        if(dirs[i] == 'U'){
-            if( y < 10){ //y가 9일때가 up할 수 있는 최대치
-                if(check[x][y][x][y+1] == false){
-                    check[x][y][x][y+1] = true;
-                    check[x][y+1][x][y] = true;
-                    answer++;
-                }
-                y++;
-            }
+        if(dirs[i] == 'R') dir = 0;
+        else if(dirs[i] == 'L') dir = 1;
+        else if(dirs[i] == 'U') dir = 2;
+        else dir = 3;
+        
+        tmp_x = x + dx[dir];
+        tmp_y = y + dy[dir];
+        
+        if(tmp_x < 0 || tmp_x > 10 || tmp_y < 0 || tmp_y > 10)// 범위 체크
+            continue;
+        
+        if(!check[x][y][tmp_x][tmp_y]){// 처음 방문한 길이라면
+            answer++;
+            check[x][y][tmp_x][tmp_y] = check[tmp_x][tmp_y][x][y] = true;
         }
-        if(dirs[i] == 'D'){
-            if( y  > 0){ //y가 1일 때가 down할 수 있는 최소치
-                if(check[x][y][x][y-1] == false){
-                    check[x][y][x][y-1] = true;
-                    check[x][y-1][x][y] = true;
-                    answer++;
-                }
-                y--;
-            }
-        }
-        if(dirs[i] == 'R'){
-            if( x < 10){ //x가 9일때가 right할 수 있는 최대치
-                if(check[x][y][x+1][y] == false){
-                    check[x][y][x+1][y] = true;
-                    check[x+1][y][x][y] = true;
-                    answer++;
-                }
-                x++;
-            }
-        }
-        if(dirs[i] == 'L'){
-            if( x > 0){ //x가 1일때가 left할 수 있는 최대치
-                if(check[x][y][x-1][y] == false){
-                    check[x][y][x-1][y] = true;
-                    check[x-1][y][x][y] = true;
-                    answer++;
-                }
-                x--;
-            }
-        }
+        y = tmp_y;
+        x = tmp_x;
     }
     return answer;
     
