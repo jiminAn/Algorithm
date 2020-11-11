5756b9552a64486a0bd708e299a7ce758a5c3382
jiminAn <54790133+jiminAn@users.noreply.github.com>
2020-11-11 21:29:55 +0900

    Update Programmers_2020_kakao_Internship2.py

--- Programmers/Programmers_2020_kakao_Internship2.py
+++ Programmers/Programmers_2020_kakao_Internship2.py
@@ -1,5 +1,6 @@
+# 우선순위가 낮은 기호를 기준으로 split()
 def calc(priority, n, expression):
-    if n == 2:
+    if n == 2: # 마지막 인덱스일 경우 기호 상관없이 eval()에서 연산 수행하면 됨
         return str(eval(expression))
     if priority[n] == '*':
         res = eval('*'.join([calc(priority, n+1, e) for e in expression.split('*')]))
@@ -9,10 +10,9 @@ def calc(priority, n, expression):
         res = eval('-'.join([calc(priority, n+1, e) for e in expression.split('-')]))
     return str(res)
 
-
 def solution(expression):
     answer = 0
-    priorities = [
+    priorities = [ # 우선순위가 낮은 순으로 [0] < [1] < [2] 리스트 저장
         ('*', '-', '+'),
         ('*', '+', '-'),
         ('+', '*', '-'),
