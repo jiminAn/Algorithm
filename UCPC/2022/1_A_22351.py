45165efe4d924360f1eaeb2d1eed238c3a7e0fde
jiminAn <als398@naver.com>
2022-05-24 16:45:33 +0900

    UCPC 2021 예선

---
+++ UCPC/2022/1_A_22351.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+S = input().strip()
+N = len(S)
+
+if N == 1:
+    print(S[0], S[0], sep=' ')
+    exit(0)
+
+for i in range(1,4): # 1,2,3자리 수
+    step = i
+    s, e = 0, i
+    next_flag = True
+    cur = int(S[0:i])
+    while e < N:
+        if cur % int('9'*step) == 0: # 끝자리일 경우 자리수 증가
+            step += 1
+        s = e
+        e = e + step
+        next = int(S[s:e])
+        if cur + 1 != next:
+            next_flag = False
+            break
+        cur = next
+    if next_flag:
+        break
+
+A = int(S[0:i])
+B = int(next)
+
+if A < B:
+    print(A, B, sep=' ')
+else:
+    print(A, A, sep=' ')
